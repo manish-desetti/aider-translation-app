99ce8d9ea03476d8c41dec7f3a634124c8fb1d39
manish <manishdesetti@gmail.com>
2023-10-09 11:02:47 +0530

    aider: Refactor translate_text function to accept target_language as an argument and update prompt accordingly.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- translate_app.py
+++ translate_app.py
@@ -5,25 +5,33 @@ import openai
 openai.api_key = "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"
 
 # Function to translate text using GPT-3.5 Turbo model
-def translate_text(text):
+def translate_text(text, target_language):
+    if target_language == "German":
+        prompt = f"Translate the following English text to German:\n\n{text}"
+    elif target_language == "French":
+        prompt = f"Translate the following English text to French:\n\n{text}"
+    else:
+        return "Invalid language selection"
+    
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"Translate the following English text to German:\n\n{text}",
+        prompt=prompt,
         max_tokens=100,
         temperature=0.7,
         n=1,
         stop=None,
-        
     )
     return response.choices[0].text.strip()
 
 # Streamlit app code
 def main():
-    st.title("English to German Translation")
+    st.title("English Translation")
     input_text = st.text_input("Enter text in English")
+    target_language = st.radio("Select target language", ("German", "French"))
+    
     if st.button("Translate"):
         if input_text:
-            translation = translate_text(input_text)
+            translation = translate_text(input_text, target_language)
             st.write("Translation:")
             st.write(translation)
         else:
